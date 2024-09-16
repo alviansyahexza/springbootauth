05a98739969743bf8d3a67491f53b120b304319c
alviansyahexza <alviansyahexza@gmail.com>
2024-09-16 09:39:25 +0700

    setup repo

---
+++ src/main/java/com/example/springbootauth/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.springbootauth.repo;
+
+import com.example.springbootauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
